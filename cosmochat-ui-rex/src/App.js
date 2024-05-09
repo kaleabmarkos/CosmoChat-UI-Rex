a845c39fba39a0b6915051fdf3fe7743aac986cd
kaleabmarkos <kaleabgessese@gmail.com>
2024-05-09 12:30:31 -0500

    first commit

---
+++ cosmochat-ui-rex/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Task 1 of the CosmoChat UI</h1>
+    </div>
+  );
+}
+
+export default App;
